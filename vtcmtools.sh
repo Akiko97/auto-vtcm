1db8fdd66d898af2accc52a9dc44bd413bc3531c
Shinonome Akiko <shinonomeakiko@gmail.com>
2019-04-28 21:47:23 +0800

    fix a bug

--- vtcmtools.sh
+++ vtcmtools.sh
@@ -138,6 +138,26 @@ function dev {
 	fi
 	download-dev
 	setenv-dev
+	VTCM_INSTALL_PATH=`pwd`
+	export CUBE_PATH=$VTCM_INSTALL_PATH/cube-1.3
+	export CUBELIBPATH=$CUBE_PATH/cubelib
+	export CUBESYSPATH=$CUBE_PATH/proc
+	export LD_LIBRARY_PATH=$CUBELIBPATH/lib/:$LD_LIBRARY_PATH
+	export CUBE_BASE_DEFINE=$CUBE_PATH/proc/main/base_define
+	export CUBE_DEFINE_PATH=$CUBE_PATH/proc/define
+	export CUBE_SYS_PLUGIN=$CUBE_PATH/proc/plugin/
+	cd cube_manage
+	export CUBEAPPPATH=`pwd` 
+	export CUBE_APP_PLUGIN=$CUBEAPPPATH/plugin/:$CUBE_APP_PLUGIN
+	export LD_LIBRARY_PATH=$CUBEAPPPATH/locallib/bin:$LD_LIBRARY_PATH
+	export CUBE_DEFINE_PATH=$CUBEAPPPATH/define:$CUBE_DEFINE_PATH
+
+	echo "CUBESYSPATH is ${CUBESYSPATH}"
+	cd locallib
+	make
+	cd ../module
+	make
+	cd $VTCM_INSTALL_PATH
 }
 
 function restorebashrc {
