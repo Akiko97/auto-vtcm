92a716ec706c989621a1779533ae839b66d8f502
Shinonome Akiko <shinonomeakiko@gmail.com>
2019-04-28 21:24:52 +0800

    add dev support

--- vtcmtools.sh
+++ vtcmtools.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 function download {
 	git clone https://github.com/biparadox/cube-1.3.git
-	git clone https://github.com/biparadox/gm_sm2_master
+	git clone https://github.com/biparadox/gm_sm2_master.git
 	git clone https://github.com/TCLab-BJUT/cube-tcm.git
+}
+
+function download-dev {
+	git clone https://github.com/biparadox/cube_manage.git
 }
 
 function patchcode {
@@ -21,21 +25,32 @@ function setuuid {
 	echo $uuid > ./cube-1.3/proc/plugin/uuid
 }
 
-function setenv {
-	cp $HOME/.bashrc $HOME/.bashrc.withoutcube
-	if grep 'cube-1.3' $HOME/.bashrc > /dev/null 2>&1
+function rmenv {
+	if grep 'VTCMENV' $HOME/.bashrc > /dev/null 2>&1
 	then
-		echo "already set cube env"
+		sed -e '/VTCMENV/d' $HOME/.bashrc > $HOME/.bashrc.tmp
+		mv $HOME/.bashrc.tmp $HOME/.bashrc
+		echo "vtcm env has been removed"
+	elif grep 'CUBEDEVENV' $HOME/.bashrc > /dev/null 2>&1
+	then
+		sed -e '/CUBEDEVENV/d' $HOME/.bashrc > $HOME/.bashrc.tmp
+		mv $HOME/.bashrc.tmp $HOME/.bashrc
+		echo "cube development env has been removed"
 	else
-		echo "# CUBE ENV" >> $HOME/.bashrc
-		echo "oldpwd=\`pwd\`" >> $HOME/.bashrc
-		echo "cd '`pwd`/cube-1.3'" >> $HOME/.bashrc
-		echo "source ./set_env.sh" >> $HOME/.bashrc
-		echo "cd '`pwd`/cube-tcm'" >> $HOME/.bashrc
-		echo "source ./set_env.sh" >> $HOME/.bashrc
-		echo "cd \$oldpwd" >> $HOME/.bashrc
-		echo "success"
+		echo "nothing to remove from .bashrc"
 	fi
+}
+
+function setenv {
+	rmenv
+	echo "# VTCMENV" >> $HOME/.bashrc
+	echo "oldpwd=\`pwd\` # VTCMENV" >> $HOME/.bashrc
+	echo "cd '`pwd`/cube-1.3' # VTCMENV" >> $HOME/.bashrc
+	echo "source ./set_env.sh # VTCMENV" >> $HOME/.bashrc
+	echo "cd '`pwd`/cube-tcm' # VTCMENV" >> $HOME/.bashrc
+	echo "source ./set_env.sh # VTCMENV" >> $HOME/.bashrc
+	echo "cd \$oldpwd # VTCMENV" >> $HOME/.bashrc
+	echo "add vtcm env to .bashrc"
 }
 
 function build {
@@ -102,21 +117,31 @@ function build {
 	setenv
 }
 
-function rmenv {
-	if grep 'cube-1.3' $HOME/.bashrc > /dev/null 2>&1
+function setenv-dev {
+	rmenv
+	echo "# CUBEDEVENV" >> $HOME/.bashrc
+	echo "oldpwd=\`pwd\` # CUBEDEVENV" >> $HOME/.bashrc
+	echo "cd '`pwd`/cube-1.3' # CUBEDEVENV" >> $HOME/.bashrc
+	echo "source ./set_env.sh # CUBEDEVENV" >> $HOME/.bashrc
+	echo "cd '`pwd`/cube_manage' # CUBEDEVENV" >> $HOME/.bashrc
+	echo "source ./set_env.sh # CUBEDEVENV" >> $HOME/.bashrc
+	echo "cd \$oldpwd # CUBEDEVENV" >> $HOME/.bashrc
+	echo "add cube development env to .bashrc"
+}
+
+function dev {
+	if [ -d cube-1.3 ]
 	then
-		if [ -e $HOME/.bashrc.withoutcube ]
-		then
-			echo "restore with backup file"
-			rm -rf $HOME/.bashrc
-			mv $HOME/.bashrc.withoutcube $HOME/.bashrc
-		else
-			echo "No backup file found, restore from /etc/skel/"
-			cp /etc/skel/.bashrc $HOME/
-		fi
+		echo "already build"
 	else
-		echo "nothing to restore"
+		build
 	fi
+	download-dev
+	setenv-dev
+}
+
+function restorebashrc {
+	cp /etc/skel/.bashrc $HOME/
 }
 
 function quit {
@@ -170,18 +195,31 @@ function initvm {
 
 function usage {
 	echo "Usage $0 [command]"
-	echo "Command:"
+	echo "Commands:"
 	echo "	build - download, patch and build vtcm, set UUID, set env"
+	echo "	dev - build development enviroment"
+	echo "	clean - clean all downloaded or modified file"
+	echo ""
 	echo "	download - just download all packages"
+	echo "	download-dev - download cube_manage"
 	echo "	setuuid - set UUID"
 	echo "	patchcode - patch source code"
-	echo "	setenv - set env in .bashrc"
+	echo "	setenv - set vtcm env to .bashrc"
+	echo "	setenv-dev - set cube development env to .bashrc"
 	echo "	rmenv - remove env from .bashrc"
 	echo "	run - run vtcm emulator and load vtcmd_dev"
 	echo "	quit - quit vtcm emulator and unload vtcmd_dev"
 	echo "	vtcmd_dev - load vtcmd_dev"
 	echo "	unvtcmd_dev - unload vtcmd_dev"
 	echo "	initvm - init virtual machine TPM"
+	echo "	restorebashrc - restore .bashrc file with system"
+}
+
+function clean {
+	quit
+	rmenv
+	rm -rf cube-1.3 gm_sm2_master cube-tcm cube_manage
+	echo "you can safely remove auto-vtcm folder"
 }
 
 if [[ $USER == "root" ]]
@@ -194,15 +232,20 @@ then
 	case "$1" in
 		build) build ;;
 		download) download ;;
+		download-dev) download-dev ;;
 		setuuid) setuuid ;;
 		patchcode) patchcode ;;
 		setenv) setenv ;;
+		setenv-dev) setenv-dev ;;
 		rmenv) rmenv ;;
 		run) run ;;
 		quit) quit ;;
 		vtcmd_dev) vtcmd_dev ;;
 		unvtcmd_dev) unvtcmd_dev ;;
 		initvm) initvm ;;
+		restorebashrc) restorebashrc ;;
+		dev) dev ;;
+		clean) clean ;;
 		*) echo "Wrong Command!"
 		   usage ;;
 	esac
